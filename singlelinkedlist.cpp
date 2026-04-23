911b9e381e0765f2ec73c0efdf83c3a24242a6af
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-23 19:24:33 +0700

    Menambahkan fungsi delNode dan traverse

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -77,3 +77,40 @@ if (START == NULL || nim <= START->noMhs)
 
         return (current != NULL);
     }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim,previous,current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+    else
+        previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+    else
+        {
+            cout << "\nData di dalam list adalah\n";
+            Node *currentNode = START;
+
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
+};
